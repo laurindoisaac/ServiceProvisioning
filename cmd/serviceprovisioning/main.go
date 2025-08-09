4952d875c2b83592fbec5e0a4b1bc5cc77f36616
Lauri <116688165+laurindoisaac@users.noreply.github.com>
2025-08-10 03:19:12 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/serviceprovisioning/main.go
@@ -0,0 +1,23 @@
+// cmd/serviceprovisioning/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "serviceprovisioning/internal/serviceprovisioning"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := serviceprovisioning.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
